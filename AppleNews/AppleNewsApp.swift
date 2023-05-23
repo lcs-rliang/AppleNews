fe37eb39ac8311f4f54f3a20ec2f9273c9cabd85
Ruiyan Liang <Jliang@lcs.on.ca>
2023-05-23 11:34:52 -0400

    Initial Commit

---
+++ AppleNews/AppleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleNewsApp.swift
+//  AppleNews
+//
+//  Created by dyhidrogen monoxide on 2023/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
