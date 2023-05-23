9936234dd03c5f7b697a00b341d30daea2b80240
Ruiyan Liang <Jliang@lcs.on.ca>
2023-05-23 11:39:51 -0400

    Reorganized project  to create Views group and NewsListView

--- AppleNews/AppleNewsApp.swift
+++ AppleNews/AppleNewsApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AppleNewsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+           NewsListView()
         }
     }
 }
---
+++ AppleNews/Views/NewsListView.swift
@@ -0,0 +1,20 @@
+//
+//  NewsListView.swift
+//  AppleNews
+//
+//  Created by dyhidrogen monoxide on 2023/5/23.
+//
+
+import SwiftUI
+
+struct NewsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsListView()
+    }
+}
